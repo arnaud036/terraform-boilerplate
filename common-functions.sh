6c230569642b89b8c3c3d03fb7fdeb533800228b
Arnaud Delabarre <delabarre.arnaud@gmail.com>
2023-05-19 15:16:31 -0700

    Initial commit with scripts

---
+++ common-functions.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+function docker_tf () {
+    local EXTRA_ARGS=""
+    local REPO_ROOT="$( cd "$(dirname "$0")" ; pwd -P )"
+    local TF_VERSION="1.3.7"
+    local TF_DOCKER_NAME="hashicorp/terraform:${TF_VERSION}"
+    
+    if [ $(docker image ls -q ${TF_DOCKER_NAME} | wc -l) -lt 1 ]; then
+        docker pull ${TF_DOCKER_NAME}
+    fi
+
+    docker run -i --rm \
+        --volume "$REPO_ROOT:/terraform-data-dir" \
+        --volume "$HOME/.aws:/root/.aws" \
+        -w "/terraform-data-dir/${TF_ENVIRONMENT_CONF}" \
+        ${EXTRA_ARGS} \
+        ${TF_DOCKER_NAME} "$@"
+}
